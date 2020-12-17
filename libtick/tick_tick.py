2bc25075629ec320b4a5a52e02259f77fabc561e
lazeroffmichael <michaeldlazeroff@gmail.com>
2020-12-16 19:38:41 -0800

    Archived list fix

--- libtick/tick_tick.py
+++ libtick/tick_tick.py
@@ -38,7 +38,7 @@ class TickTickClient:
         self.access_token = ''
         self.cookies = {}
         self.login(username, password)
-        self.lists = self.get_lists_name_and_id()
+        self.lists = self.get_lists_name_and_body()
 
     def login(self, username: str, password: str) -> None:
         """
@@ -76,6 +76,15 @@ class TickTickClient:
         if response.status_code != 200:
             raise RuntimeError(error_message)
 
+    @logged_in
+    def get_initial_login_checkpoint_summary(self):
+        url = self.BASE_URL + 'batch/check/0'
+        response = httpx.get(url, cookies=self.cookies)
+        self.check_status_code(response, 'Could Not Retrieve Values')
+        response = response.json()
+
+
+
     def create_task(self, task_name: str):
         pass
 
@@ -83,6 +92,7 @@ class TickTickClient:
         pass
 
     def get_inbox_id(self):
+        url = self.BASE_URL + 'batch/check/0'
         pass
 
     @logged_in
@@ -94,7 +104,6 @@ class TickTickClient:
         :param list_type: Defaults to normal "TASK" type, or specify 'NOTE' for note type list
         :return: Dictionary containing {list_name:list_id_value}
         """
-        """Creates a new list with the passed parameters"""
         if list_name in self.lists:
             raise ValueError(f"Cannot Create List '{list_name}' -> Already Exists")
 
@@ -117,8 +126,19 @@ class TickTickClient:
         }
         response = httpx.post(url, json=payload, cookies=self.cookies)
         self.check_status_code(response, 'Could Not Create List')
-        self.lists = self.get_lists_name_and_id()
+        self.lists = self.get_lists_name_and_body()
         return {list_name: self.lists[list_name]}
+
+    @logged_in
+    def create_list_folder(self, folder_name: str) -> str:
+        """
+        Creates a list folder
+        :param folder_name: Name of the folder
+        :return: String declaring success
+        """
+        # Folder names can be reused
+
+
 
     @logged_in
     def delete_list(self, list_name: str) -> str:
@@ -133,22 +153,36 @@ class TickTickClient:
 
         url = self.BASE_URL + 'batch/project'
         payload = {
-            'delete': [self.lists[list_name]],
+            'delete': [self.lists[list_name]['id']],
         }
         response = httpx.post(url, json=payload, cookies=self.cookies)
         self.check_status_code(response, 'Could Not Delete List')
         # Update class project dictionary
-        self.lists = self.get_lists_name_and_id()
+        self.lists = self.get_lists_name_and_body()
         return f"List: '{list_name}' Deleted"
 
     @logged_in
     def archive_list(self, list_name: str) -> str:
         """
-        Moves the passed list to "Archived Lists"
+        Moves the passed list to "Archived Lists" instead of deleting
         :param list_name: Name of the list to be moved
         :return: String specifying the archive was successful
         """
+        # Check if the name exists
+        if list_name not in self.lists:
+            raise KeyError(f"List: '{list_name}' Does Not Exist To Archive")
 
+        # Check if the list is already archived
+        if self.lists[list_name]['closed'] is False or self.lists[list_name]['closed'] is None:
+            url = self.BASE_URL + 'batch/project'
+            self.lists[list_name]['closed'] = True
+            payload = {
+                'update': [self.lists[list_name]]
+            }
+            response = httpx.post(url, json=payload, cookies=self.cookies)
+            self.check_status_code(response, 'Could Not Archive List')
+
+        return f"List: '{list_name}' is Archived"
 
     @logged_in
     def get_lists(self) -> list:
@@ -163,19 +197,31 @@ class TickTickClient:
         return response.json()
 
     @logged_in
-    def get_lists_name_and_id(self) -> dict:
+    def get_lists_name_and_body(self) -> dict:
         """
         Maps each list (project) name to its respective id.
         :return: Dictionary
         """
         request = self.get_lists()
         name_dict = dict()
-        for task in request:
-            name = task['name']
-            task_id = task['id']
-            name_dict[name] = task_id
+        for i in range(len(request)):
+            project_name = request[i]['name']
+            body = request[i]
+            name_dict[project_name] = body
 
         return name_dict
+
+    @logged_in
+    def get_list_folders(self) -> dict:
+        pass
+
+    @logged_in
+    def get_initial_login_checkpoint_summary(self):
+        url = self.BASE_URL + 'batch/check/0'
+        response = httpx.get(url, cookies=self.cookies)
+        self.check_status_code(response, 'Could Not Retrieve Values')
+
+        return response.json()
 
     @logged_in
     def get_summary(self, time_zone: str, start_date: datetime, end_date: datetime = None, full_day: bool = True) -> list:
@@ -255,3 +301,4 @@ if __name__ == '__main__':
     passw = os.getenv('TICKTICK_PASS')
     client = TickTickClient(usern, passw)
     print(client.lists)
+    print(client.get_list_folders())
--- tests/test_lists.py
+++ tests/test_lists.py
@@ -8,15 +8,20 @@ class TestLists:
         """Tests creating a list and having it populate the class list dictionary"""
         name = 'Created List from Python'
         color = "#e070ff"
-        assert name in client.create_list(name, color)
+        client.create_list(name, color)
         assert name in client.lists
+        kind = client.lists[name]['kind']
+        assert kind == 'TASK'
 
     def test_create_list_type_note_success(self, client):
         """Tests creating a list with type note"""
         name = 'Test Note Type List'
         color = "#b20000"
-        assert name in client.create_list(name, color)
+        list_type = 'NOTE'
+        client.create_list(name, color, list_type)
         assert name in client.lists
+        kind = client.lists[name]['kind']
+        assert kind == 'NOTE'
 
     def test_create_list_duplicate_failure(self, client):
         """Tests no list creation if the same name already exists"""
@@ -27,12 +32,12 @@ class TestLists:
     def test_get_lists(self, client):
         """Tests getting lists works"""
         returned_list = client.get_lists()
-        assert returned_list != ''
+        assert len(returned_list) != 0
 
     def test_get_lists_id_and_name(self, client):
         """Tests proper parsing of the class.list dictionary"""
         name = 'Created List from Python'
-        returned_dictionary = client.get_lists_name_and_id()
+        returned_dictionary = client.get_lists_name_and_body()
         assert name in returned_dictionary
 
     def test_delete_list_pass(self, client):
@@ -76,8 +81,14 @@ class TestLists:
         client.create_list(name)
         # Archive the test list
         client.archive_list(name)
-
-        pass
+        assert client.lists[name]['closed'] is True
+        # Delete the test list
+        client.delete_list(name)
 
     def test_archive_list_failure(self, client):
-        pass
+        name = 'Fake Archive List'
+        with pytest.raises(KeyError):
+            client.archive_list(name)
+
+    def test_get_list_folders(self, client):
+        """Retruned list should be good"""
