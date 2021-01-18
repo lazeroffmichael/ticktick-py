f008add21403b3e3d28e753e49871a4846f286fc
lazeroffmichael <56806733+lazeroffmichael@users.noreply.github.com>
2021-01-18 13:35:06 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: PyPy',
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
