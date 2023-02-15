49a9ba0331d6b2ce9ea03761b892fe03035db2b7
Benjamin Weigt <benjamin_weigt@web.de>
2023-02-15 11:13:54 +0100

    sync habits to client state

--- ticktick/managers/habits.py
+++ ticktick/managers/habits.py
@@ -1,4 +1,5 @@
 class HabitManager:
+    HABITS_BASE_URL = 'https://api.ticktick.com/api/v2/habits'
 
     def __init__(self, client_class):
         self._client = client_class
@@ -11,3 +12,15 @@ class HabitManager:
 
     def update(self):
         pass
+
+    def get_all(self):
+        """
+        Gets all current habit-information for the user.
+
+        client.state['habits'] should have this information stored after login,
+        only use this method to obtain habits if the client state is out of sync!
+
+        Returns:
+            httpx: The response from the get request.
+        """
+        return self._client.http_get(url=self.HABITS_BASE_URL, cookies=self._client.cookies, headers=self._client.HEADERS)
